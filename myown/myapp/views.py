670ef1f04ab6e55f1b31eff023d7234cc4cd384c
meshach567 <arinzemeshach123@gmail.com>
2024-08-27 12:31:12 +0100

    commit

--- myown/myapp/views.py
+++ myown/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    context = {
+        'name': 'Amaka',
+        'age': '24',
+        'place': 'Lagos'
+        }
+    return render(request, 'index.html', context)
