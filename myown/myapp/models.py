50dab0bb09329911bbe3803a6dc4d1dc3e805b8e
meshach567 <arinzemeshach123@gmail.com>
2024-08-28 16:07:05 +0100

    commit

--- myown/myapp/models.py
+++ myown/myapp/models.py
@@ -2,13 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Feature:
+class Box:
     id: int
     name: str
     details: str
 
-class Box:
-    id: int
-    icon: str
-    name: str
-    details: str
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)  # Provide a default value
--- myown/myapp/views.py
+++ myown/myapp/views.py
@@ -12,16 +12,33 @@ def index(request):
     #     'place': 'Lagos'
     #     }
 
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.details = 'Our Service'
-
     box1 = Box()
     box1.id = 0
-    box1.icon = '<i class="bi bi-buildings"></i>'
-    box1.name = 'Easy to Used'
-    return render(request, 'index.html', { 'feature': feature1})
+    box1.name = 'Fast'
+    box1.details = 'Our Service'
+
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Easy to Used'
+    feature1.details = 'Our Service'
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Easy to implement'
+    feature2.details = 'Our Service'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Easy to work with'
+    feature3.details = 'Our Service'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Easy to integrate'
+    feature4.details = 'Our Service'
+
+    features = [feature1, feature2, feature3, feature4]
+    return render(request, 'index.html', { 'box': box1, 'features': features})
 
 # def counter(request):
 #     text = request.POST['text']
