d2399c883924f5706f52c6f026062b9bc8754580
meshach567 <arinzemeshach123@gmail.com>
2024-08-28 12:18:50 +0100

    commmit

--- myown/myapp/models.py
+++ myown/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Feature:
+    id: int
+    name: str
+    details: str
+
+class Box:
+    id: int
+    icon: str
+    name: str
+    details: str
--- myown/myapp/views.py
+++ myown/myapp/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature, Box
+
 
 # Create your views here.
 
 def index(request):
-    context = {
-        'name': 'Amaka',
-        'age': '24',
-        'place': 'Lagos'
-        }
-    return render(request, 'index.html', context)
+    # context = {
+    #     'name': 'Amaka',
+    #     'age': '24',
+    #     'place': 'Lagos'
+    #     }
+
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our Service'
+
+    box1 = Box()
+    box1.id = 0
+    box1.icon = '<i class="bi bi-buildings"></i>'
+    box1.name = 'Easy to Used'
+    return render(request, 'index.html', { 'feature': feature1})
 
 # def counter(request):
 #     text = request.POST['text']
