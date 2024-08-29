fc9d7030df1260d9bd5e72cda247f7b91ed663e9
meshach567 <arinzemeshach123@gmail.com>
2024-08-29 11:06:34 +0100

    commit

--- myown/myapp/urls.py
+++ myown/myapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
     # path('counter', views.counter, name='counter')
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login')
 ]
